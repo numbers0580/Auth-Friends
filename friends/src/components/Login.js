676572a256ac250061e8adac9d729bde8453302f
numbers0580 <koreankid18@yahoo.com>
2020-07-22 05:08:21 -0600

    Fixed login issue, able to get token now, trying to build form to add friends.

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,8 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {axiosWithAuth} from '../utils/axiosWithAuth';
+import {useHistory} from 'react-router-dom';
+//import Loader from 'react-loader-spinner';
+
+const blankFields = {
+    username: '',
+    password: ''
+};
 
 const Login = () => {
+    const [entries, setEntries] = useState(blankFields);
+    const history = useHistory();
+
+    const updateEntries = e => {
+        const name = e.target.name;
+        const value = e.target.value;
+
+        setEntries({...entries, [name]: value});
+    };
+
+    const submitLogin = e => {
+        e.preventDefault();
+
+        axiosWithAuth()
+            .post('/api/login', entries)
+            .then(posting => {
+                console.log('posting.data is', posting.data);
+                console.log('posting.data.payload is', posting.data.payload);
+                localStorage.setItem('token', posting.data.payload);
+                history.push('/friends');
+            })
+            .catch(loginError => {
+                console.log('Error with login');
+            })
+
+        setEntries(blankFields);
+    };
+
     return (
-        <div>Login Page</div>
+        <form onSubmit={submitLogin}>
+            <div>
+                <div><label>Username:</label></div>
+                <div><input type='text' name='username' value={entries.username} onChange={updateEntries} /></div>
+            </div>
+            <div>
+                <div><label>Password:</label></div>
+                <div><input type='password' name='password' value={entries.password} onChange={updateEntries} /></div>
+            </div>
+            <div>
+                <button>Login!</button>
+            </div>
+        </form>
     );
 };
 
--- friends/src/utils/axiosWithAuth.js
+++ friends/src/utils/axiosWithAuth.js
@@ -1,9 +1,18 @@
 import axios from 'axios';
 
 export const axiosWithAuth = () => {
+    
     const token = localStorage.getItem('token');
     return axios.create({
         headers: {Authorization: token},
         baseURL: 'http://localhost:5000'
     });
+    
+
+    /*
+    const token = JSON.parse(window.localStorage.getItem('token'));
+    return axios.create({
+        headers: {Authorization: token}
+    });
+    */
 };
